fddd6c076a9f119a9b47ecae5e2bb7075b20646c
sohibjonabdialimov <sohibjonabdialimov6@gmail.com>
2022-12-07 21:36:10 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+/* typing animation */
+var typed = new Typed(".typing",{
+    strings:["Web Designer","JavaScript Developer","Graphic Designer","YouTuber"],
+    typeSpeed:200,
+    BackSpeed:200,
+    loop: true
+})
